1749a97171656546deeb6c788eda6a0260fd075c
Oleg G <verdrehung@gmail.com>
2011-08-22 23:32:39 +0800

    1.3 -> 1.4

--- LWP-Protocol-socks/lib/LWP/Protocol/socks.pm
+++ LWP-Protocol-socks/lib/LWP/Protocol/socks.pm
@@ -2,7 +2,7 @@
 package LWP::Protocol::http::socks;
 require LWP::Protocol::http;
 our @ISA = qw(LWP::Protocol::http);
-our $VERSION = "1.3";
+our $VERSION = "1.4";
 LWP::Protocol::implementor('http::socks' => 'LWP::Protocol::http::socks');
 
 sub new {
