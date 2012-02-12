533fd2a2b548b87f70f2acb45669bec07cdb31d4
Sheridan Rawlins <sheridan.rawlins@yahoo.com>
2012-02-12 09:51:58 -0800

    Bump version -> 1.6

--- LWP-Protocol-socks/lib/LWP/Protocol/socks.pm
+++ LWP-Protocol-socks/lib/LWP/Protocol/socks.pm
@@ -2,7 +2,7 @@
 package LWP::Protocol::http::socks;
 require LWP::Protocol::http;
 our @ISA = qw(LWP::Protocol::http);
-our $VERSION = "1.5";
+our $VERSION = "1.6";
 LWP::Protocol::implementor('http::socks' => 'LWP::Protocol::http::socks');
 
 sub new {
